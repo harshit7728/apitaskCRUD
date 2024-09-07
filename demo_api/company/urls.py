ba5187f17ab5a1efc0167c3f002d48a463c51191
Harshit Panchal <143997202+harshit7728@users.noreply.github.com>
2024-09-07 18:22:21 +0530

    Delete demo_api directory

--- demo_api/company/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import CreateCompanyView, CreateUserView, FileUploadView
-
-urlpatterns = [
-    path('create-company/', CreateCompanyView.as_view(), name='create-company'),
-    path('create-user/', CreateUserView.as_view(), name='create-user'),
-    path('create-upload/', FileUploadView.as_view(), name='upload-user'),
-]
